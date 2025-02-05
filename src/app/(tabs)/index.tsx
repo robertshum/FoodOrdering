a39dba2ab328bfa35a83136a889faa48c8c98d6b
robertshum <robertshum@gmail.com>
2025-02-04 23:56:09 -0800

    first custom component

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,45 +1,14 @@
-import { StyleSheet, Image } from 'react-native';
-import { Text, View } from '@/components/Themed';
-import Colors from '@/constants/Colors';
+import { View } from '@/components/Themed';
+import ProductListItem from '@/components/ProductListItem';
+// temp data
 import products from '../../../assets/data/products';
 
-// Temp data
-const product = products[0];
-
-export default function TabOneScreen() {
+export default function MenuScreen() {
   return (
-    <View style={styles.container}>
-      <Image style={styles.image} source={{ uri: product.image }} />
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price.toFixed(2)}</Text>
+    <View>
+      <ProductListItem product={products[0]}></ProductListItem>
+      <ProductListItem product={products[1]}></ProductListItem>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    padding: 10,
-    borderRadius: 20,
-    overflow: 'hidden',
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: '600',
-    marginVertical: 10,
-    alignSelf: 'center',
-  },
-  price: {
-    color: Colors.light.tint,
-    fontWeight: 'bold',
-    marginTop: 10,
-  },
-  image: {
-    width: '100%',
-    aspectRatio: 1,
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
---
+++ src/components/ProductListItem.tsx
@@ -0,0 +1,44 @@
+import { StyleSheet, Image } from 'react-native';
+import { Text, View } from '@/components/Themed';
+import Colors from '@/constants/Colors';
+import products from '../../assets/data/products';
+
+const ProductListItem = ({ product }) => {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.image} source={{ uri: product.image }} />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price.toFixed(2)}</Text>
+    </View>
+  );
+};
+
+export default ProductListItem;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    borderRadius: 20,
+    overflow: 'hidden',
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: '600',
+    marginVertical: 10,
+    alignSelf: 'center',
+  },
+  price: {
+    color: Colors.light.tint,
+    fontWeight: 'bold',
+    marginTop: 10,
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 1,
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
