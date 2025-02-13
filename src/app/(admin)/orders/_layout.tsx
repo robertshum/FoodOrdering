8b3eba77224dac514a5f2c5885d481cd1d057d8a
robertshum <robertshum@gmail.com>
2025-02-13 00:26:20 -0800

    add material top tab navigator

--- src/app/(admin)/orders/_layout.tsx
+++ src/app/(admin)/orders/_layout.tsx
@@ -2,6 +2,6 @@ import { Stack } from 'expo-router';
 
 export default function OrdersLayout() {
   return <Stack>
-    <Stack.Screen name="index" options={{ title: 'Orders' }} />
+    <Stack.Screen name="list" options={{ headerShown: false }} />
   </Stack>;
 };
